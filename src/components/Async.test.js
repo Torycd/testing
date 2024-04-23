c48efb151dbd79fa2c779645ad758cb755f66c7f
Torycd <vicidundun@gmail.com>
2024-04-23 09:35:29 +0100

    yes

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -1,12 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import Async from "./Async";
 
-describe('Assigned component', () => {
+
+describe('Async component', () => {
     test('render posts if request succeds', async() => {
         window.fetch = jest.fn()
-        window.fetch.mockResolveValueOnce({json: async () => [{id: 'p1', title: 'First post'}]})
+        window.fetch.mockResolvedValueOnce({json: async () => [{id: 'p1', title: 'First post'}]})
         render(<Async/>)
-        const listItemElements =  await screen.findAllByRole('listitem')
+        const listItemElements =  await screen.findAllByRole('listi
         expect(listItemElements).not.toHaveLength(0)
     })
 })
